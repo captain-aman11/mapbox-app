bcfb25d3f39c4109ef06e6c3cda13d854b97f689
captain-aman11 <kmraman11011@gmail.com>
2022-03-03 03:57:55 +0000

    tooltip fix

--- src/components/ToolTip.js
+++ src/components/ToolTip.js
@@ -1,23 +1,19 @@
 import React from "react";
+import MouseTooltip from "react-sticky-mouse-tooltip";
 
 function ToolTip({ currentState }) {
   if (!currentState) return null;
+
   return (
     <>
-      <div
-        className="tooltip"
-        style={{
-          left: parseFloat(currentState.x + 10),
-          top: parseFloat(currentState.y + 10),
-          position: "absolute",
-          zIndex: 8,
-          backgroundColor: "#fff",
-          padding: "4px",
-          transition: "all 0.7 ease-in-out",
-        }}
+      <MouseTooltip
+        style={{ backgroundColor: "#fff", padding: " 0px 4px" }}
+        visible={true}
+        offsetX={10}
+        offsetY={10}
       >
         <p>{`${currentState.properties.NAME_1}, ${currentState.properties.NAME_0}`}</p>
-      </div>
+      </MouseTooltip>
     </>
   );
 }
